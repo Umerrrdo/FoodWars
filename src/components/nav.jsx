bdacea6568e8b44eb59aef48835088b60cf9811d
Umer Mehmood <umehmood.bscs21seecs@seecs.edu.pk>
2024-04-06 16:36:13 +0500

    config settings done

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,8 +1,11 @@
+import logo from '../assets/logo.png';
+
 export default function Nav() {
     return (
         <nav>
             <div className="logo">
-                <h1 >Umer Mehmood</h1>
+                <img src={logo} alt="Food Wars" />
+                <h1>Umer Mehmood</h1>
             </div>
         </nav>
     );
