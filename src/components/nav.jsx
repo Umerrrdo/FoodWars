46213a69c63796b3849c84007f908befd53fb010
Umer Mehmood <umehmood.bscs21seecs@seecs.edu.pk>
2024-04-06 16:02:46 +0500

    init

---
+++ src/components/nav.jsx
@@ -0,0 +1,9 @@
+export default function Nav() {
+    return (
+        <nav>
+            <div className="logo">
+                <h1 >Umer Mehmood</h1>
+            </div>
+        </nav>
+    );
+}
