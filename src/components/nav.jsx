1ceaa4b5a37a7667359133d1ae3c21dc897bc359
Umer Mehmood <umehmood.bscs21seecs@seecs.edu.pk>
2024-04-21 17:27:59 +0500

    config-tailwind

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,11 +1,19 @@
 import logo from '../assets/logo.png';
+import 'tailwindcss/tailwind.css';
 
 export default function Nav() {
     return (
-        <nav>
-            <div className="logo">
-                <img src={logo} alt="Food Wars" />
-                <h1>Umer Mehmood</h1>
+        <nav className='flex flex-row justify-between w-full'>
+            <div className="logo ">
+                <img className='h-20 p-2 relative left-4 rounded-lg' src={logo} alt="Food Wars" />
+            </div>
+            <div >
+                <ul className='flex flex-row relative top-5 text-base'>
+                    <a href="#"><li className='px-4'>Home</li></a>
+                    <a href="#"><li className='px-4'>Our Deals</li></a>
+                    <a href="#"><li className='px-4'>About</li></a>
+                    <a href="#"><li className='px-4'>Contact</li></a>
+                </ul>
             </div>
         </nav>
     );
